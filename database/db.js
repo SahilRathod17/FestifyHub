d0a7a87667e0d91ac881fa47c6ad90c4d6d593ed
iamSahil <samael17111@gmail.com>
2024-03-08 14:30:42 +0530

    config :fix4

--- database/db.js
+++ database/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 const Connection = async (username, password) => {
   const URL = `mongodb+srv://${username}:${password}@cmpanytest.sxlnuzd.mongodb.net/`;
+
+  console.log("MongoDB Connection URL:", URL);
+  
   try {
     await mongoose.connect(URL, {
       useUnifiedTopology: true,
