ffed3f7984100a1b2202467b8c2d8c075c4b9727
iamSahil <samael17111@gmail.com>
2024-03-08 13:26:37 +0530

    config :fix2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const password = process.env.DB_PASSWORD;
 const port = process.env.PORT;
 
 Connection(username, password);
-app.listen(port, '0.0.0.0/0', () =>
+app.listen(port, '0.0.0.0', () =>
   console.log(`Server is running successfully on PORT ${port}`)
 );
 
